d55eae1c6d5452ceb269334002a1c7096fd94414
Garyy12 <90533473+Garyy12@users.noreply.github.com>
2024-02-12 18:44:49 -0600

    Add files via upload

---
+++ listado-letras-af/b.php
@@ -0,0 +1,46 @@
+<?php
+header("Content-Type: application/xls");
+header("Content-Disposition: attachment; filename= Listado-Afiliados-b.xls");
+
+require '../../php/conexion.php';
+$conexion = $conectar;
+?>
+  <table>
+                <thead>
+                    <tr>
+                        <th> Nombre <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Apellido paterno <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Apellido materno <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Direccion <span class="icon-arrow">&UpArrow;</span></th>
+                        <th> Profesión <span class="icon-arrow">&UpArrow;</span></th>
+						<th> Descripción <span class="icon-arrow">&UpArrow;</span></th>
+                    </tr>
+                </thead>
+                <tbody>
+               
+                 
+				<?php 
+				$sql="SELECT *  from afiliados where a_paterno like 'B%'";
+				$result=mysqli_query($conexion,$sql);
+
+				while($mostrar=mysqli_fetch_array($result)){
+				 ?>
+				<tr>
+						
+                        <td> <?php echo $mostrar ['nombre']; ?> </td>
+                        <td> <?php echo $mostrar ['a_paterno']; ?> </td>
+                        <td> <?php echo $mostrar ['a_materno']; ?></td>
+                        <td>
+                            <p class="status pending "><?php echo $mostrar ['direccion']; ?></p>
+                        </td>
+                        <td> <strong><?php echo $mostrar ['profesion']; ?></strong> </td>
+						<td> <strong><?php echo $mostrar ['descrip']; ?></strong> </td>
+		
+		
+				</tr>
+					<?php 
+					}
+					 ?>
+                
+                </tbody>
+            </table>
---
+++ listado-letras/l.php
@@ -0,0 +1,45 @@
+<?php
+header("Content-Type: application/xls");
+header("Content-Disposition: attachment; filename= Listado-Usuarios-l.xls");
+
+require '../../php/conexion.php';
+$conexion = $conectar;
+?>
+ <table>
+                <thead>
+                    <tr>
+                        <th> Usuario </th>
+                        <th> Nombre </th>
+                        <th> Apellido paterno </th>
+                        <th> Apellido materno </th>
+                        <th> Email </th>
+                        <th> Telefono </th>
+						<th> Nivel </th>
+                    </tr>
+                </thead>
+                <tbody>
+               
+                 
+				<?php 
+				$sql="SELECT *  from usuarios where admin = 0 and a_paterno like 'L%'";
+				$result=mysqli_query($conexion,$sql);
+
+				while($mostrar=mysqli_fetch_array($result)){
+				 ?>
+				<tr>
+						<td> <?php echo $mostrar ['usuario']; ?> </td>
+                        <td> <?php echo $mostrar ['nombre']; ?> </td>
+                        <td> <?php echo $mostrar ['a_paterno']; ?> </td>
+                        <td> <?php echo $mostrar ['a_materno']; ?></td>
+                        <td>
+                            <p class="status pending "><?php echo $mostrar ['email']; ?></p>
+                        </td>
+                        <td> <strong><?php echo $mostrar ['num_tel']; ?></strong> </td>
+						<td> <strong><?php echo $mostrar ['id_nivel']; ?></strong> </td>
+				</tr>
+					<?php 
+					}
+					 ?>
+                
+                </tbody>
+            </table>
